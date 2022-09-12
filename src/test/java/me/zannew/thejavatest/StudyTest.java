d3c71a5d7531fe99afa10b6b6e59ad3ae3f9540c
“zannew“ <wonny21292@gmail.com>
2022-09-12 16:54:55 +0900

    feature: 테스트 반복하기 - @RepeatedTest와 @ParameterizedTest
    - 반복 테스트별 변수, 인덱스 넘버 등 사용

--- src/test/java/me/zannew/thejavatest/StudyTest.java
+++ src/test/java/me/zannew/thejavatest/StudyTest.java
@@ -9,7 +9,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -28,6 +32,20 @@ class StudyTest {
 	@SlowTest
 	void create_study_slow() {
 		System.out.println("create study slow");
+	}
+
+	@DisplayName("반복해서 테스트하기")
+	@RepeatedTest(value = 10, name = "{displayName}, {currentRepetition} / {totalRepetitions}")
+	void repeatTest(RepetitionInfo repetitionInfo) {
+		System.out.println(
+			"repeat test! " + repetitionInfo.getCurrentRepetition() + " / " + repetitionInfo.getTotalRepetitions());
+	}
+
+	@DisplayName("파라미터라이즈드 테스트하기")
+	@ParameterizedTest(name = "{index} - {displayName} message={0}")
+	@ValueSource(strings = {"가을모기", "짱싫", "살려주세여", "흡혈모기"})
+	void parameterizedTest(String message) {
+		System.out.println(message);
 	}
 
 	@BeforeAll
