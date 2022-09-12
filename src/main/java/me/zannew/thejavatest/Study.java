418977d9bb5db58ef733bb90412388e8a332f0a1
“zannew“ <wonny21292@gmail.com>
2022-09-12 15:40:58 +0900

    JUnit5 assertThrows : 예외를 발생시켜 에러 메시지를 확인

--- src/main/java/me/zannew/thejavatest/Study.java
+++ src/main/java/me/zannew/thejavatest/Study.java
@@ -6,6 +6,9 @@ public class Study {
 	private int limit;
 
 	public Study(int limit) {
+		if (limit < 0) {
+			throw new IllegalArgumentException("limit 은 0보다 커야 한다.");
+		}
 		this.limit = limit;
 	}
 
--- src/test/java/me/zannew/thejavatest/StudyTest.java
+++ src/test/java/me/zannew/thejavatest/StudyTest.java
@@ -18,15 +18,10 @@ class StudyTest {
 	@Test
 	@DisplayName("스터디 생성하기")
 	void create_study() {
-		Study study = new Study(1);
 
-		// Functional interface(Executable) 전달
-		assertAll(
-			() -> assertNotNull(study),
-			() -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
-				() -> "스터디를 처음 생성하면 상태값이 " + StudyStatus.DRAFT + "여야 한다."),
-			() -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-		);
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+			() -> new Study(-10));
+		assertEquals("limit 은 0보다 커야 한다.", exception.getMessage());
 	}
 
 	@BeforeAll
