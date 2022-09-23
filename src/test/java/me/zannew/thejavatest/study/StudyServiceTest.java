2eeadd8c9bc72e2bf439061f5e49f121ef5219d8
“zannew“ <wonny21292@gmail.com>
2022-09-23 21:05:02 +0900

    test: Mockito 사용 객체 생성
    - 어노테이션 없이 직접 메서드로 인자로 넘길 mock 객체 생성하여 테스트

--- src/test/java/me/zannew/thejavatest/study/StudyServiceTest.java
+++ src/test/java/me/zannew/thejavatest/study/StudyServiceTest.java
@@ -1,4 +1,22 @@
+package me.zannew.thejavatest.study;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import me.zannew.thejavatest.member.MemberService;
+
 class StudyServiceTest {
-  
+
+	@DisplayName("mock 객체를 인자로 사용하여 StudyService 인스턴스를 성공적으로 생성한다.")
+	@Test
+	void createStudyService() {
+
+		MemberService memberService = mock(MemberService.class);
+		StudyRepository studyRepository = mock(StudyRepository.class);
+		StudyService studyService = new StudyService(memberService, studyRepository);
+		assertNotNull(studyService);
+	}
 }
