a6b1c664c399ebfc2c2daae39162da252a177722
“zannew“ <wonny21292@gmail.com>
2022-09-24 20:21:43 +0900

    test: 어노테이션 사용하여 mock 객체 생성 테스트

--- src/test/java/me/zannew/thejavatest/study/StudyServiceTest.java
+++ src/test/java/me/zannew/thejavatest/study/StudyServiceTest.java
@@ -5,10 +5,19 @@ import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import me.zannew.thejavatest.member.MemberService;
 
+@ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
+
+	@Mock
+	MemberService memberService;
+	@Mock
+	StudyRepository studyRepository;
 
 	@DisplayName("mock 객체를 인자로 사용하여 StudyService 인스턴스를 성공적으로 생성한다.")
 	@Test
@@ -19,4 +28,19 @@ class StudyServiceTest {
 		StudyService studyService = new StudyService(memberService, studyRepository);
 		assertNotNull(studyService);
 	}
+
+	@DisplayName("어노테이션 사용하여 mock 객체 생성하여 StudyService 인스턴스를 성공적으로 생성한다.")
+	@Test
+	void createStudyServiceWithAnnotation() {
+		StudyService studyService = new StudyService(memberService, studyRepository);
+		assertNotNull(studyService);
+	}
+
+	@DisplayName("어노테이션을 테스트 메서드 파라미터로 직접 사용할 수 있다.")
+	@Test
+	void createStudyServiceWithAnnotation2(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
+		StudyService studyService = new StudyService(memberService, studyRepository);
+		assertNotNull(studyService);
+	}
+
 }
