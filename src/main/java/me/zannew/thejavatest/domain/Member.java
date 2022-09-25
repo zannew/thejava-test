da28463eb65ba97333bbe95ec4ffd8ccbfa746e7
“zannew“ <wonny21292@gmail.com>
2022-09-25 23:49:26 +0900

    feature: Member 클래스 생성

--- src/main/java/me/zannew/thejavatest/domain/Member.java
+++ src/main/java/me/zannew/thejavatest/domain/Member.java
@@ -1,2 +1,23 @@
-package me.zannew.thejavatest.domain;public class Member {
+package me.zannew.thejavatest.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String email;
+
 }
