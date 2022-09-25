97386e54f09ba763dcd66d4e0d9552f77c4dba32
“zannew“ <wonny21292@gmail.com>
2022-09-26 00:52:42 +0900

    feature: Study 클래스 수정

--- src/main/java/me/zannew/thejavatest/domain/Study.java
+++ src/main/java/me/zannew/thejavatest/domain/Study.java
@@ -1,20 +1,35 @@
 package me.zannew.thejavatest.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import me.zannew.thejavatest.StudyStatus;
 
+@Entity
+@NoArgsConstructor
 @Setter
 @Getter
 public class Study {
 
-	private StudyStatus status;
-	private int limit;
+	@GeneratedValue
+	@Id
+	private Long id;
+	private StudyStatus status = StudyStatus.DRAFT;
+	private int limitCount;
 	private String name;
+	private LocalDateTime openedDateTime;
+	@ManyToOne
 	private Member owner;
 
 	public Study(int limit, String name) {
-		this.limit = limit;
+		this.limitCount = limit;
 		this.name = name;
 	}
 
@@ -22,28 +37,6 @@ public class Study {
 		if (limit < 0) {
 			throw new IllegalArgumentException("limit 은 0보다 커야 한다.");
 		}
-		this.limit = limit;
-	}
-
-	public StudyStatus getStatus() {
-		return this.status;
-	}
-
-	public int getLimit() {
-		return this.limit;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public String toString() {
-		return "Study{" +
-			"status=" + status +
-			", limit=" + limit +
-			", name='" + name + '\'' +
-			", owner=" + owner +
-			'}';
+		this.limitCount = limit;
 	}
 }
