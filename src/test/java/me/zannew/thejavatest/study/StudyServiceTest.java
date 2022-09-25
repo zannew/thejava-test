286146f87412ee9bbe4662a75a5837c5ea555842
“zannew“ <wonny21292@gmail.com>
2022-09-26 00:55:58 +0900

    test: Stubbing 케이스별 테스트 코드 작성
    - 특정 파라미터인 경우 결과 stubbing
    - 모든 파라미터의 경우 결과 stubbing
    - void 타입 메서드 특정 파라미터인 경우 예외 stubbing
    - 호출 순서에 따라 다른 결과 stubbing

--- src/test/java/me/zannew/thejavatest/study/StudyServiceTest.java
+++ src/test/java/me/zannew/thejavatest/study/StudyServiceTest.java
@@ -3,12 +3,15 @@ package me.zannew.thejavatest.study;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import me.zannew.thejavatest.domain.Member;
 import me.zannew.thejavatest.member.MemberService;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,7 +25,6 @@ class StudyServiceTest {
 	@DisplayName("mock 객체를 인자로 사용하여 StudyService 인스턴스를 성공적으로 생성한다.")
 	@Test
 	void createStudyService() {
-
 		MemberService memberService = mock(MemberService.class);
 		StudyRepository studyRepository = mock(StudyRepository.class);
 		StudyService studyService = new StudyService(memberService, studyRepository);
@@ -43,4 +45,78 @@ class StudyServiceTest {
 		assertNotNull(studyService);
 	}
 
+	@Test
+	void beforeStubbingTest() {
+		Optional<Member> member = memberService.findById(1L);
+		memberService.validate(2L);
+	}
+
+	@DisplayName("특정 파라미터인 경우 원하는 member 객체를 리턴한다.")
+	@Test
+	void stubbingMember() {
+
+		StudyService studyService = new StudyService(memberService, studyRepository);
+		assertNotNull(studyService);
+
+		Member member = new Member();
+		member.setId(1L);
+		member.setEmail("woni@woni.com");
+
+		when(memberService.findById(1L)).thenReturn(Optional.of(member));
+
+		assertEquals("woni@woni.com", memberService.findById(1L).get().getEmail());
+	}
+
+	@DisplayName("어떤 파라미터를 전달해도 원하는 member 객체를 리턴한다.")
+	@Test
+	void stubbingMemberWithAny() {
+
+		StudyService studyService = new StudyService(memberService, studyRepository);
+		assertNotNull(studyService);
+
+		Member member = new Member();
+		member.setId(1L);
+		member.setEmail("woni@woni.com");
+
+		when(memberService.findById(any())).thenReturn(Optional.of(member));
+
+		assertEquals("woni@woni.com", memberService.findById(1L).get().getEmail());
+		assertEquals("woni@woni.com", memberService.findById(2L).get().getEmail());
+		assertEquals("woni@woni.com", memberService.findById(3L).get().getEmail());
+	}
+
+	@DisplayName("void 타입 특정 파라미터를 전달하는 경우만 예외를 던진다.")
+	@Test
+	void doThrowWhenVoidType() {
+		doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			memberService.validate(1L);
+		});
+
+		memberService.validate(2L);
+	}
+
+	@DisplayName("호출 순서에 따라 다른 결과를 발생시킨다.")
+	@Test
+	void differentResultByCallOrder() {
+
+		StudyService studyService = new StudyService(memberService, studyRepository);
+		assertNotNull(studyService);
+
+		Member member = new Member();
+		member.setId(1L);
+		member.setEmail("woni@woni.com");
+
+		when(memberService.findById(any()))
+			.thenReturn(Optional.of(member))
+			.thenThrow(new IllegalArgumentException())
+			.thenReturn(Optional.empty());
+
+		assertEquals("woni@woni.com", memberService.findById(1L).get().getEmail());
+		assertThrows(IllegalArgumentException.class, () -> {
+			memberService.findById(2L);
+		});
+		assertEquals(Optional.empty(), memberService.findById(3L));
+	}
 }
