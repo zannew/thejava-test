0ab95b899bab63e1115799f53db4de9cb80a1733
“zannew“ <wonny21292@gmail.com>
2022-09-25 23:49:36 +0900

    feature: Study 클래스 생성

--- src/main/java/me/zannew/thejavatest/domain/Study.java
+++ src/main/java/me/zannew/thejavatest/domain/Study.java
@@ -1,10 +1,17 @@
-package me.zannew.thejavatest;
+package me.zannew.thejavatest.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import me.zannew.thejavatest.StudyStatus;
+
+@Setter
+@Getter
 public class Study {
 
 	private StudyStatus status;
 	private int limit;
 	private String name;
+	private Member owner;
 
 	public Study(int limit, String name) {
 		this.limit = limit;
@@ -36,6 +43,7 @@ public class Study {
 			"status=" + status +
 			", limit=" + limit +
 			", name='" + name + '\'' +
+			", owner=" + owner +
 			'}';
 	}
 }
